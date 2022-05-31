f2d33877e4243b0f8ecf5dfe1fa5a96ab13507d5
Erik Tholen <erik.tholen@vinngroup.se>
2022-05-31 23:22:12 +0200

    Build ShowsView

--- src/app/views/ShowsView/index.tsx
+++ src/app/views/ShowsView/index.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import Centered from "../../components/Centered";
+import Divider from "../../components/Divider";
+import Page from "../../components/Page";
+import Section from "../../components/Section";
+import Title from "../../components/Title";
+import ShowSearch from "../../components/ShowSearch";
+import ShowList from "../../components/ShowList";
+
+import {Show} from "../../interfaces/Show";
 
 const ShowsView = () => {
-  return <div>Shows</div>
+  const [shows, setShows] = useState<Show[]>([]);
+
+  return (
+    <Page>
+      <Title>Shows</Title>
+      <Divider />
+      <Section>
+        <Centered>
+          <ShowSearch onChange={setShows} />
+        </Centered>
+      </Section>
+      <Section>
+        <ShowList shows={shows} />
+      </Section>
+    </Page>
+  );
 };
 
 export default ShowsView;
